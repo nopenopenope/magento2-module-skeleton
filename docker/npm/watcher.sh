e7a77508bf8da55468e4b28fdab1fa00e0cf4655
Maximilian Graf Schimmelmann <max@schimmelmann.org>
2023-06-20 22:56:47 +0200

    Add Composer Package for NPM.

---
+++ docker/npm/watcher.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+grunt clean
+grunt exec
+grunt less
+grunt watch
--- docker/php/etc/install-magento.sh
+++ docker/php/etc/install-magento.sh
@@ -1,20 +1,29 @@
 #!/usr/bin/env bash
 
+rm -rf /var/www/html/app/etc/env.php
+
 php bin/magento setup:install \
   --base-url="http://www.magento-skeleton.test:3500/" \
+  --admin-email="developer@dev.test" \
+  --admin-firstname="Test" \
+  --admin-lastname="Test" \
+  --admin-password="test123!@#" \
+  --admin-user="admin" \
+  --backend-frontname="admin" \
+  --currency="EUR" \
   --db-host="database" \
   --db-name="magento2" \
   --db-password="magento2" \
-  --db-user="root" \
-  --admin-firstname="Admin" \
-  --admin-lastname="Admin" \
-  --admin-email="developer@dev.test" \
-  --admin-user="admin" \
-  --admin-password="test123!@#" \
-  --use-rewrites="1" \
-  --backend-frontname="admin" \
+  --db-prefix="" \
+  --db-user="magento2" \
+  --language="en_US" \
   --opensearch-host="opensearch" \
   --opensearch-port="9200" \
-  --use-sample-data
+  --timezone="Europe/Amsterdam" \
+  --use-rewrites="1" \
+  --use-sample-data \
+  --cleanup-database
 
 php bin/magento module:disable Magento_AdminAdobeImsTwoFactorAuth Magento_TwoFactorAuth
+php bin/magento deploy:mode:set developer
+
